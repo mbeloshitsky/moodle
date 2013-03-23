34a5494565dca6ccc68594bf35bc89afd027a1bf
Michel Beloshitsky <mbeloshitsky@gmail.com>
2013-03-23 07:39:57 +0000

    Начало работы над персонифицированной темой для УрГУПС. Поправлены цвета на более соответствующие корпоративным. Добавлены часы с индикацией четности недели.

---
+++ theme/arialist_usurt/javascript/clock.js
@@ -0,0 +1,19 @@
+function addNulls (val) {
+	return (val < 10 ? '0' : '') + val
+}
+
+function displayCurrentTime () {
+	var weekDays = ['Пнд', 'Втр', 'Срд', 'Чтв', 'Птн', 'Сбт', 'Вск'];
+	var timeWidget = document.getElementById('clock');
+	var now = new Date();
+	var nowYear = new Date('1.1.'+now.getFullYear());
+	var currWeek = Math.floor((Number(now) - Number(nowYear))/(7*86400*1000));
+
+	timeWidget.innerHTML = now.getHours() + ':' + addNulls(now.getMinutes()) + ', ' 
+		+ (weekDays[now.getDay()]) + ', ' 
+		+ (currWeek % 2 == 0 ? '<span class="even">Четная</span>' : '<span class="odd">Нечетная</span>') + ', ' 
+		+ now.getDate() + '.' + addNulls(now.getMonth()+1) + '.' + now.getFullYear()+'г.';
+}
+setInterval(displayCurrentTime, 10000);
+
+document.addEventListener('DOMContentLoaded', displayCurrentTime);
